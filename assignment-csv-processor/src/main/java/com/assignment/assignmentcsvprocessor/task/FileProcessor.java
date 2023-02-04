e7b12df409ffdd6f82716dbb9310b00ee42dfce3
kw <kevin.wu1@nhs.net>
2023-02-04 19:44:47 +0000

    merged into a single class, fixed TDD tests

--- assignment-csv-processor/src/main/java/com/assignment/assignmentcsvprocessor/task/FileProcessor.java
+++ assignment-csv-processor/src/main/java/com/assignment/assignmentcsvprocessor/task/FileProcessor.java
@@ -1,12 +1,22 @@
 package com.assignment.assignmentcsvprocessor.task;
 
 import com.assignment.assignmentcsvprocessor.dto.CustomerDetailDto;
+import com.assignment.assignmentcsvprocessor.exception.CSVFormatException;
 import com.assignment.assignmentcsvprocessor.service.CustomerDataApiService;
-import com.assignment.assignmentcsvprocessor.service.FileLocator;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
 
 @Service
 @Slf4j
@@ -15,15 +25,16 @@ public class FileProcessor {
     @Autowired
     private CustomerDataApiService dataApiService;
 
-    @Autowired
-    private FileLocator fileLocator;
+    @Value("${file.folder}")
+    private String folder;
+
+    private static final String[] HEADERS = { "CUSTOMER_REF", "CUSTOMER_NAME", "CUSTOMER_ADDRESS_1", "CUSTOMER_ADDRESS_2", "TOWN", "COUNTY", "COUNTRY", "POSTCODE"};
 
     public boolean execute(){
 
-        Iterable<CSVRecord> records = fileLocator.readCSVFile();
         log.info("Starting to process file");
 
-        for (CSVRecord record : records) {
+        for (CSVRecord record : readCSVFile()) {
 
             try {
                 CustomerDetailDto request = CustomerDetailDto.builder().customerRef(record.get("CUSTOMER_REF"))
@@ -36,6 +47,7 @@ public class FileProcessor {
                         .postcode(record.get("POSTCODE")).build();
 
 
+                // TODO could handle failure case better
                 dataApiService.createNewCustomer(request);
 
             }catch (Exception e){
@@ -46,5 +58,32 @@ public class FileProcessor {
 
         // TODO move file to a different location or delete it after is processed
         return true;
+    }
+
+    private Iterable<CSVRecord> readCSVFile() {
+        // Read file
+        BufferedReader in =null;
+        try (Stream<Path> walk = Files.walk(Paths.get(folder))) {
+
+            List<String> result = walk.map(Path::toString)
+                    .filter(f -> f.endsWith(".csv")).toList();
+            log.info("Total number of files in documents [{}]", result.size());
+            if(!result.isEmpty()) {
+                Path filePath = Paths.get(result.get(0));
+
+                log.info("Reading file [{}]", filePath.getFileName());
+                in = Files.newBufferedReader(filePath);
+
+                return CSVFormat.DEFAULT
+                        .withHeader(HEADERS)
+                        .withFirstRecordAsHeader()
+                        .parse(in);
+            }
+
+        } catch (IOException e) {
+            throw new CSVFormatException("Exception while reading CSV file - error message - {}", e);
+        }
+
+        return null;
     }
 }
--- assignment-csv-processor/src/test/java/com/assignment/assignmentcsvprocessor/task/FileProcessorTest.java
+++ assignment-csv-processor/src/test/java/com/assignment/assignmentcsvprocessor/task/FileProcessorTest.java
@@ -1,33 +1,54 @@
 package com.assignment.assignmentcsvprocessor.task;
 
+import com.assignment.assignmentcsvprocessor.service.CustomerDataApiService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.HttpServerErrorException;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 class FileProcessorTest {
 
-    final FileProcessor processor = new FileProcessor();
+    @InjectMocks
+    private FileProcessor processor;
 
+    @Mock
+    CustomerDataApiService dataApiService;
+
+    @BeforeEach
+    void setup(){
+        ReflectionTestUtils.setField(processor, "folder", getClass().getClassLoader().getResource("test.csv").getPath());
+    }
     @Test
     void testFileExecuteWithSuccess(){
         // given
+        given(dataApiService.createNewCustomer(any())).willReturn(true);
 
-        // when
+        // when then
         assertTrue(processor.execute());
-        // then
+        Mockito.verify(dataApiService, times(2)).createNewCustomer(any());
     }
 
     @Test
     void testFileExecuteWithFailure(){
         // given
+        given(dataApiService.createNewCustomer(any())).willThrow(HttpServerErrorException.class);
 
-        // when
+        // when then
         assertFalse(processor.execute());
-
-        // then
+        Mockito.verify(dataApiService, times(1)).createNewCustomer(any());
     }
 }
